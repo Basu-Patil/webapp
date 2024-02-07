cb52535496f4b44d8fa8476445b5fcf8caf57d31
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-07 18:43:58 -0500

    Added new middleware to check serve connection

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import health_check_router from "./healthCheckroute.js";
 import { onlyGetMethodAllowed } from "../middlewares/methodNotAllowed.js";
 import { noQueryParams } from "../middlewares/checkheaders.js";
+import { healthCheck } from "../middlewares/checkServerStatus.js";
 import userRouter from "./userRoute.js";
 
 const registerRouter = (app) => {
-    app.use("/healthz",onlyGetMethodAllowed, noQueryParams, health_check_router);
-    app.use("/v1/user", noQueryParams, userRouter);
+    app.use("/healthz",healthCheck, onlyGetMethodAllowed, noQueryParams, health_check_router);
+    app.use("/v1/user",healthCheck, noQueryParams, userRouter);
 }
 
 export default registerRouter;
