fbe14a30ac31287ac7cacb7a76e35181473a85ee
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-07 14:19:22 -0500

    Revert "Add test cases to test method not allowed middleware"
    This reverts commit 9a2dda26dd6d6fa1a33917d9859a6c30057ba574.

---
+++ test/servicelayer/userservice.test.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+
+
+describe('User Service', () => {
+    it('Test Case 1: ', async () => {
+        // Test implementation
+        expect(true).equal(true);
+        
+    });
+});
