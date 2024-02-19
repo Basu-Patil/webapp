72ab06da3e41d464316ec0bc8536a0fa7910f115
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-19 01:23:29 -0500

    Add scripts and packer code

---
+++ packer/scripts/post-deploy-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ---------------------------------------Post Deploy Setup---------------------------------------
+
+# go to the project directory
+cd ~/projects/webapp/
+
+# create a .env file
+touch .env
+
+# write the environment variables to the .env file, take values from the packer template
+cat <<EOF > .env
+PORT=${PORT}
+MYSQL_HOST=${MYSQL_HOST}
+MYSQL_USER=${MYSQL_USER}
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
+MYSQL_DATABASE=${MYSQL_DATABASE}
+DIALECT=${DIALECT}
+MYSQL_PORT=${MYSQL_PORT}
+EOF
+
+# start the application
+# npm start
