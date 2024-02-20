5cc5cb0dfb5bd9c40b0240428d6e9a3281378a6a
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-20 16:16:49 -0500

    systemd file

--- packer/scripts/systemd-setup.sh
+++ packer/scripts/systemd-setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
-
+sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+sudo chown root:root /etc/systemd/system/webapp.service
+sudo chmod 644 /etc/systemd/system/webapp.service
 sudo systemctl daemon-reload
-
-# Enable services
 sudo systemctl enable webapp.service
