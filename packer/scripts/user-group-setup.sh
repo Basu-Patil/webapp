924e4f368e7fcbb1537e7dc3be47b31a2821940c
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-21 04:02:49 -0500

    updated user group sh

--- packer/scripts/user-group-setup.sh
+++ packer/scripts/user-group-setup.sh
@@ -5,12 +5,12 @@ sudo groupadd csye6225
 
 sudo useradd -m -g csye6225 -s /sbin/nologin csye6225
 
-sudo chown -R csye6225 /home/patilbasavaraj293/projects/*
+sudo chown -R csye6225 /home/*
 
-sudo chmod -R u+x /home/patilbasavaraj293/projects/*
+sudo chmod -R u+x /home/*
 
-sudo chgrp -R csye6225 /home/patilbasavaraj293/projects/*
+sudo chgrp -R csye6225 /home/*
 
 # # change the ownership of the /webapp directory to the csye6225 user and group
-# sudo chown -R csye6225:csye6225 ~/projects/webapp
+# sudo chown -R csye6225:csye6225 ~/webapp
 
