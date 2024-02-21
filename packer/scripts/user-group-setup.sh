61c4b536a8a83b0e144af9d93091b2b63fb14299
Basavaraj Patil <patil.ba@northeastern.edu>
2024-02-21 04:18:28 -0500

    updated sh files

--- packer/scripts/user-group-setup.sh
+++ packer/scripts/user-group-setup.sh
@@ -9,8 +9,6 @@ sudo chown -R csye6225 /home/*
 
 sudo chmod -R u+x /home/*
 
-sudo chgrp -R csye6225 /home/*
-
 # # change the ownership of the /webapp directory to the csye6225 user and group
 # sudo chown -R csye6225:csye6225 ~/webapp
 
