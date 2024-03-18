65f7b88a7a3eb48d02cb3f231269920eb699aa13
basupatil1213 <patil.ba@northeastern.edu>
2024-03-18 18:55:03 -0400

    Add ops-agent shell

--- logger/webappLogger.js
+++ logger/webappLogger.js
@@ -6,20 +6,19 @@ dotenv.config();
 const { combine, timestamp, printf, json } = format;
 
 
-// const webappLogFormat = printf(({ level, message, timestamp }) => {
-//     return `${timestamp} : ${message}`;
-// });
-
-const logDirName = process.env.LOG_DIR_NAME || '/var/log/webapp';
-
 const webappLogger = createLogger({
     level: 'debug',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         json()
     ),
-    transports: [
-        new transports.File({ filename: `${process.env.logDirName}/webapp.log`, level: 'debug' }),
-    ],
+    transports: [],
 })
+
+if(process.env.NODE_ENV === 'test'){
+    webappLogger.add( new transports.Console());
+}
+else{
+    webappLogger.add( new transports.File({ filename: '/var/log/webapp/webapp.log', level: 'debug' }));
+}
 export default webappLogger;
